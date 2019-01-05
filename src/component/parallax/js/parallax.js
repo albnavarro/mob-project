cd6a5b10b05252caca9329324892cb899c9593a9
albertonav <alberto.navaro@gmail.com>
2019-01-05 23:18:40 +0100

    [ PARALLAX ] Add smooth js option

--- src/component/parallax/js/parallax.js
+++ src/component/parallax/js/parallax.js
@@ -5,19 +5,33 @@ class parallaxClass {
       this.$ = {
         $parallaxItem: $('.parallax__item'),
         itemArray: [],
-        transformProperty: Modernizr.prefixed('transform')
+        transformProperty: Modernizr.prefixed('transform'),
+
+        // SMOOTH JS
+        smoothCss: 'CSS',
+        smoothJs: 'JS',
+        smoothType: '',
+        duration: 1000,
+        req: null
       }
       parallaxClass.instance = this;
     }
     return parallaxClass.instance;
   }
 
+
+
+
   init(){
+    this.$.smoothType = this.$.smoothJs
     eventManager.push('scroll', this.linearParallax.bind(this));
     eventManager.push('scrollThrottle', this.smoothParallax.bind(this));
     eventManager.push('resize', this.updateArray.bind(this));
     eventManager.push('load', this.buildData.bind(this));
   }
+
+
+
 
   buildData(){
     function obj(item) {
@@ -25,6 +39,7 @@ class parallaxClass {
       this.container = item.closest('.parallax__container')
       this.offset = 0
       this.diff = 0
+      this.startValue = 0
       this.height = 0
       this.velocity = (this.item.attr('data-velocity') || 2 )
       this.align = (this.item.attr('data-align') || 'top' )
@@ -44,15 +59,23 @@ class parallaxClass {
       this.$.itemArray[this.$.itemArray.length-1].calcOffset()
       this.$.itemArray[this.$.itemArray.length-1].calcHeight()
 
-      if(this.$.itemArray[this.$.itemArray.length-1].ease == 'smooth') {
-        this.$.itemArray[this.$.itemArray.length-1].item.addClass('smooth-transition')
+      if(this.$.itemArray[this.$.itemArray.length-1].ease == 'smooth' &&  this.$.smoothType == this.$.smoothCss) {
+          this.$.itemArray[this.$.itemArray.length-1].item.addClass('smooth-transition')
       }
     })
 
     for( let element of this.$.itemArray ) {
       this.executeParallax(element)
     }
+
+    if (this.$.smoothType == this.$.smoothJs) {
+      if( this.$.req ) cancelAnimationFrame(this.$.req);
+      this.$.req = requestAnimationFrame(this.onReuqestAnim.bind(this))
+    }
   }
+
+
+
 
   updateArray() {
     for( let element of this.$.itemArray ) {
@@ -60,7 +83,15 @@ class parallaxClass {
       element.calcHeight();
       this.executeParallax(element)
     }
+
+    if (this.$.smoothType == this.$.smoothJs) {
+      if( this.$.req ) cancelAnimationFrame(this.$.req);
+      this.$.req = requestAnimationFrame(this.onReuqestAnim.bind(this))
+    }
   }
+
+
+
 
   linearParallax() {
     for( let element of this.$.itemArray ) {
@@ -68,13 +99,76 @@ class parallaxClass {
     }
   }
 
+
+
+
   smoothParallax() {
     for( let element of this.$.itemArray ) {
-        if(element.ease == 'smooth') this.executeParallax(element)
+
+      // Se è un item con ease smooth in css calcolo ivalori e li applico
+      if(element.ease == 'smooth' && this.$.smoothType == this.$.smoothCss) {
+        this.executeParallax(element)
+      }
+
+      // Se è un item con ease smotth in js calcolo ivalori e non li applico
+      if (element.ease == 'smooth' && this.$.smoothType == this.$.smoothJs) {
+        this.executeParallax(element,false)
+      }
+    }
+
+    // Se uso lo smooth js faccio partire il loop di RAF
+    // Al suo interno verranno filtrati tutti gli elementi senza easing
+    if (this.$.smoothType == this.$.smoothJs) {
+      if( this.$.req ) cancelAnimationFrame(this.$.req);
+      this.$.req = requestAnimationFrame(this.onReuqestAnim.bind(this))
     }
   }
 
-  executeParallax(element) {
+
+  easing (n){
+    n *= 2;
+    if (n < 1) return 0.5 * n * n;
+    return - 0.5 * (--n * (n - 2) - 1);
+  }
+
+  // RAF
+  onReuqestAnim(timeStamp) {
+    let _this = this,
+        _timeStamp = parseInt(timeStamp),
+        start = _timeStamp,
+        end = start + this.$.duration;
+
+    function draw(timeStamp) {
+      let _timeStamp = parseInt(timeStamp)
+
+      for( let element of _this.$.itemArray ) {
+        ((element) => {
+           if(element.ease == 'linear') return;
+
+            let partial = _this.easing(((_timeStamp - start) / _this.$.duration)),
+            val = parseInt(element.startValue + (element.diff - element.startValue) * partial),
+            x = parseInt((element.diff - element.startValue)/20);
+
+            val +=  Math.floor(x);
+            element.startValue = val;
+            element.item.css(_this.setStyle(element,val));
+
+        })(element)
+      }
+
+      if(_timeStamp > end) return;
+
+      if( _this.$.req ) cancelAnimationFrame(_this.$.req);
+      _this.$.req = requestAnimationFrame(draw)
+    }
+
+    draw(timeStamp)
+  }
+
+
+
+  executeParallax(element,applyStyle = true) {
+
     // Esegui i colcoli solo se l'lemento è visibile nello schemro
     if( element.offset + element.height > eventManager.scrollTop()
       && element.offset < eventManager.scrollTop() + eventManager.windowsHeight()) {
@@ -101,27 +195,35 @@ class parallaxClass {
           break;
       }
 
+      if (!applyStyle) return;
 
-      let style = {};
-
-      switch(element.direction ) {
-        case 'vertical':
-          style[this.$.transformProperty] = `translate3d(0,0,0) translateY(${element.diff}px)`;
-          break;
-
-        case 'horizontal':
-          style[this.$.transformProperty] = `translate3d(0,0,0) translateX(${element.diff}px)`;
-          break;
-
-        case 'rotate':
-          style[this.$.transformProperty] = `translate3d(0,0,0) rotate(${element.diff}deg)`;
-          break;
-
-      }
-
-      element.item.css(style);
+      element.item.css(this.setStyle(element,element.diff));
     }
   }
+
+
+
+  setStyle(element,val) {
+    let style = {};
+
+    switch(element.direction ) {
+      case 'vertical':
+        style[this.$.transformProperty] = `translate3d(0,0,0) translateY(${val}px)`;
+        break;
+
+      case 'horizontal':
+        style[this.$.transformProperty] = `translate3d(0,0,0) translateX(${val}px)`;
+        break;
+
+      case 'rotate':
+        style[this.$.transformProperty] = `translate3d(0,0,0) rotate(${val}deg)`;
+        break;
+    }
+
+    return style
+  }
+
+
 
   recalcPosition() {
       this.updateArray();
