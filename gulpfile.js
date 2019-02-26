023c435b4cdce1cc021509970d86cfbabb2eb3bf
albertonav <alberto.navaro@gmail.com>
2019-02-26 21:35:28 +0100

    Refine gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const
   isProd = process.env.NODE_ENV === 'production',
 
+  // manifest     = require('./dist/manifest.json'),
   path = require('path'),
   config = require('./config.json'),
   concat = require('gulp-concat'),
@@ -27,6 +28,11 @@ const
   imagemin = require('gulp-imagemin'),
   pug = require('gulp-pug'),
   babel = require("gulp-babel"),
+  rev = require('gulp-rev'),
+  revdel = require('rev-del'),
+  del = require('del'),
+  merge = require('gulp-merge-json'),
+  replace = require('gulp-string-replace'),
   reload = browserSync.reload,
 
   themePath = path.resolve('src'),
@@ -38,13 +44,17 @@ const
   scssPath = path.join(themePath, 'scss'),
   svgPath = path.join(themePath, 'svg'),
 
-  cssPath = path.join(destPath, 'assets/css'),
+  cssDest = path.join(destPath, 'assets/css'),
   jsDest = path.join(destPath, 'assets/js'),
   svgDest = path.join(destPath, 'assets/svg'),
   imgDest = path.join(destPath, 'assets/img'),
 
-  cssFile = `${cssPath}/main.css`,
-  cssCritical = `${cssPath}/critical.css`,
+  distPath = path.join(destPath, 'assets/dist'),
+  dataMerged = path.join(destPath, 'assets/data'),
+
+  cssFile = `${cssDest}/main.css`,
+  jsFile = `${jsDest}/main.js`,
+  cssCritical = `${cssDest}/critical.css`,
   imgFiles = `${imgPath}/*`,
   jsFiles = `${jsPath}/**/*.js`,
   componentJsFiles = `${componentPath}/**/*.js`,
@@ -53,15 +63,20 @@ const
   svgFiles = `${svgPath}/*.svg`,
   pugFiles = `${themePath}/*.pug`,
   allPugFiles = `${themePath}/**/*.pug`,
+  dataFiles = `${themePath}/data/**/*.json`,
+  dataDest = `${dataMerged}/data.json`,
+  manifestFile = `${distPath}/manifest.json`
 
-  dataFile = `${themePath}/data/data.json`
 
 
+
+
+// BROWSER SYNC
 
 function browser_sync(done) {
   browserSync.init({
     proxy: config.host,
-    open: true
+    open: false
   })
 
   done();
@@ -74,8 +89,13 @@ function reloadPage(done) {
 };
 
 
-function style(done) {
-  gulp.src(path.join(scssPath, 'main.scss'))
+
+
+
+// SASS
+
+function style() {
+  return gulp.src(path.join(scssPath, 'main.scss'))
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'nested',
@@ -91,17 +111,18 @@ function style(done) {
       includeContent: false,
       sourceRoot: scssPath
     })))
-    .pipe(gulpif(isProd, cssmin({
-      keepSpecialComments: false,
-    })))
-    .pipe(gulp.dest(cssPath))
+    .pipe(gulp.dest(cssDest))
     .pipe(browserSync.stream({
       match: '**/*.css'
     }))
-
-  done();
 };
 
+
+
+
+
+
+// JS
 
 function minifyAssetsLoading() {
   return gulp.src(path.join(jsPath, 'async-assets-loading.js'))
@@ -111,8 +132,8 @@ function minifyAssetsLoading() {
 };
 
 
-function js(done) {
-  gulp.src([
+function js() {
+  return gulp.src([
       path.join(jsPath, 'base/debounce.js'),
       path.join(jsPath, 'base/raf.js'),
       path.join(jsPath, 'base/throttle.js'),
@@ -140,26 +161,34 @@ function js(done) {
     .pipe(concat('main.js'))
     .pipe(wrap('(function($){<%= contents %>})(jQuery)'))
     .pipe(babel())
-    .pipe(gulpif(isProd, uglify()))
     .pipe(gulp.dest(jsDest))
 
-  done();
 };
 
+
+
+
+
+// SVG
 
 function icons() {
   return gulp.src(svgFiles)
     .pipe(svgmin({
-        plugins: [{
-          removeViewBox: false
-        }]
-      }
-    ))
+      plugins: [{
+        removeViewBox: false
+      }]
+    }))
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(gulp.dest(svgDest))
 }
+
+
+
+
+
+// CRITICAL CSS
 
 function initializeCritical(done) {
   fs.writeFile(cssCritical, '', done);
@@ -177,8 +206,8 @@ function criticalCss(done) {
       minify: true,
       width: 1024,
       height: 768,
-      css: `${cssPath}/main.css`,
-      dest: `${cssPath}/critical.css`,
+      css: `${cssDest}/main.css`,
+      dest: `${cssDest}/critical.css`,
       include: ['.lighbox', '.parallax-container', '.parallax-item']
     });
 
@@ -187,8 +216,21 @@ function criticalCss(done) {
 };
 
 
+
+
+// PUG
+
+function mergeJson() {
+
+  return gulp.src([dataFiles, manifestFile])
+    .pipe(merge({
+      fileName: 'data.json'
+    }))
+    .pipe(gulp.dest(dataMerged));
+}
+
 function html() {
-  const jsonData = JSON.parse(fs.readFileSync(dataFile))
+  const jsonData = JSON.parse(fs.readFileSync(dataDest))
 
   return gulp.src(pugFiles)
     .pipe(pug({
@@ -199,6 +241,10 @@ function html() {
 };
 
 
+
+
+// IMAGE
+
 function image() {
   return gulp.src(imgFiles)
     .pipe(imagemin())
@@ -206,10 +252,75 @@ function image() {
 };
 
 
+
+
+// PRODUCTION FUNCTION
+
+function cleanDist() {
+  return del([
+    path.join(distPath, '*'),
+    path.join(distPath, '*.*')
+  ]);
+}
+
+function uglyfyCss() {
+  return gulp.src(cssFile)
+    .pipe(cssmin({
+      keepSpecialComments: false,
+    }))
+    .pipe(gulp.dest(cssDest))
+}
+
+function uglyfyJs() {
+  return gulp.src(path.join(jsDest, 'main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest))
+}
+
+function normalizeManifest() {
+  return gulp.src(manifestFile)
+    .pipe(replace('main.css', 'maincss'))
+    .pipe(replace('main.js', 'mainjs'))
+    .pipe(gulp.dest(distPath))
+}
+
+function dist() {
+  return gulp.src([cssFile, jsFile])
+
+    .pipe(rev())
+    .pipe(gulp.dest(distPath))
+    .pipe(rev.manifest({
+      path: 'manifest.json'
+    }))
+    .pipe(revdel(distPath))
+    .pipe(gulp.dest(distPath))
+}
+
+
+// REMOVE ALL GENERATED FILES
+
+function cleanAll() {
+  return del([
+    path.join(cssDest, '**/*.*'),
+    path.join(distPath, '*'),
+    path.join(distPath, '*.*'),
+    path.join(dataMerged, '*.*'),
+    path.join(imgDest, '*.*'),
+    path.join(destPath, '*.html'),
+    path.join(svgDest, '*.*'),
+    path.join(destPath, 'assets/js/async-assets-loading.min.js'),
+    path.join(destPath, 'assets/js/main.js'),
+    jsFile
+  ]);
+}
+
+
+// LIVE RELOAD
+
 function watch_files(done) {
   gulp.watch([scssFiles, componentscssFiles], style)
   gulp.watch([jsFiles, componentJsFiles], gulp.series(js, reloadPage))
-  gulp.watch([allPugFiles, dataFile], gulp.series(html, reloadPage))
+  gulp.watch([allPugFiles, dataFiles], gulp.series(mergeJson, html, reloadPage))
 
   done();
 }
@@ -219,12 +330,50 @@ function watch_files(done) {
 gulp.task("initializeCritical", initializeCritical)
 gulp.task("style", style)
 gulp.task("js", js)
+gulp.task("mergeJson", mergeJson)
 gulp.task("html", html)
 gulp.task("image", image)
 gulp.task("icons", icons)
 gulp.task("minifyAssetsLoading", minifyAssetsLoading)
-gulp.task("criticalCss", gulp.series(style, criticalCss))
+gulp.task("cleanDist", cleanDist)
+gulp.task("dist", dist)
+gulp.task("uglyfyCss", uglyfyCss)
+gulp.task("uglyfyJs", uglyfyJs)
+gulp.task("normalizeManifest", normalizeManifest)
+gulp.task("cleanAll", cleanAll)
 
-gulp.task("init", gulp.series(initializeCritical, icons, image, minifyAssetsLoading, style, js, html))
-gulp.task("assets", gulp.parallel(style, js));
-gulp.task('watch', gulp.parallel(browser_sync, watch_files))
+
+// MAIN TASK
+
+gulp.task("init", gulp.series(
+  initializeCritical,
+  icons,
+  image,
+  minifyAssetsLoading,
+  style,
+  js,
+  cleanDist,
+  uglyfyCss,
+  uglyfyJs,
+  dist,
+  normalizeManifest,
+  mergeJson,
+  html
+))
+
+gulp.task('watch', gulp.parallel(
+  browser_sync,
+  watch_files))
+
+gulp.task("criticalCss", gulp.series(
+  style,
+  criticalCss))
+
+gulp.task('prod', gulp.series(
+  cleanDist,
+  uglyfyCss,
+  uglyfyJs,
+  dist,
+  normalizeManifest,
+  mergeJson,
+  html))
