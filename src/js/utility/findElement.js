ac49d5e296e1b682bd4226d4038f053bed3fe2da
albertonav <alberto.navaro@gmail.com>
2020-11-16 22:02:09 +0100

    [ FInd element ] Remove jquery

--- src/js/utility/findElement.js
+++ src/js/utility/findElement.js
@@ -1,16 +1,17 @@
-export function findElement(el) {
+export function findElement(elClass) {
     return new Promise((res, reject) => {
         let start = null;
         const step = (timestamp) => {
             if (!start) start = timestamp;
             const progress = timestamp - start;
-            const $el = $(`${el}`);
+            const el = document.querySelectorAll(elClass);
+            console.log(el)
 
-            if ($el.length) {
+            if (el.length) {
                 res();
             }
 
-            if (progress < 3000 && $el.length == 0) {
+            if (progress < 3000 && el.length == 0) {
                 window.requestAnimationFrame(step);
             } else {
                 reject();
