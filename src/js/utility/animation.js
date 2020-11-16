4d993acdfa55b5cd45f58696c4ce2d0ea0da3ab4
albertonav <alberto.navaro@gmail.com>
2020-11-16 22:01:55 +0100

    [ ANIMATION ] Add porpierties

--- src/js/utility/animation.js
+++ src/js/utility/animation.js
@@ -1,16 +1,16 @@
 import {gsap} from "gsap";
 
-export function slideUp(target, duration = .5) {
+export function slideUp(target, duration = .5, ease = 'Power4.easeOut') {
     return new Promise((res, reject) => {
-        gsap.to(target, {height: 0, duration: duration, onComplete:res});
+        gsap.to(target, {height: 0, ease: ease, duration: duration, onComplete:res});
     });
 }
 
-export function slideDown(target, duration = .5) {
+export function slideDown(target, duration = .5, ease = 'Power4.easeOut') {
     return new Promise((res, reject) => {
         target.style.height = null;
         const height = target.offsetHeight;
         target.style.height = 0;
-        gsap.to(target, {height: height, duration: duration, onComplete:res});
+        gsap.to(target, {height: height, ease: ease, duration: duration, onComplete:res});
     });
 }
