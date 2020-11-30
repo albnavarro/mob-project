89bd564cd03fe6f6fc8d608d6870ee5fba71ef9b
albertonav <alberto.navaro@gmail.com>
2020-11-30 21:37:15 +0100

    [ THREEGALLERY] Add swap clone

--- src/component-vanilla/threeGallery/js/tGalleryItemClass.js
+++ src/component-vanilla/threeGallery/js/tGalleryItemClass.js
@@ -6,7 +6,7 @@ export class tGalleryItemClass {
     constructor(container) {
         this.container = container;
         this.items = container.querySelectorAll('.tGallery__item');
-        this.activeDirection = 'dx';
+        this.activeHDirection = 'dx';
         this.center = position(container).left + outerWidth(container) / 2;
     }
 
@@ -39,11 +39,17 @@ export class tGalleryItemClass {
         if (item.classList.contains('tGallery__item--active')) return;
 
         // reset lastSwapItem ( item that change layout position)
-        const prevSwapItem = this.container.querySelector('.swap-item');
+        const prevSwapItem = this.container.querySelector('.t-swap-item');
         if (typeof(prevSwapItem) != 'undefined' && prevSwapItem != null) {
-            prevSwapItem.classList.remove('swap-item')
-            prevSwapItem.classList.remove('swap-item--formLeft')
-            prevSwapItem.classList.remove('swap-item--formRight')
+            prevSwapItem.classList.remove('t-swap-item')
+            prevSwapItem.classList.remove('t-swap-item--formLeft')
+            prevSwapItem.classList.remove('t-swap-item--formRight')
+        }
+
+        // Remove swapCLone
+        const clonedSwap = this.container.querySelector('.t-clone');
+        if (typeof(prevSwapItem) != 'undefined' && prevSwapItem != null) {
+            clonedSwap.remove()
         }
 
         const itemPosX = offset(item).left;
@@ -51,6 +57,7 @@ export class tGalleryItemClass {
         const currentInnerElement = item.querySelector('.tGallery__item__wrap');
         let itemNotActive = this.container.querySelectorAll('.tGallery__item:not(.tGallery__item--active)');
         let itemNotActiveArray = Array.from(itemNotActive);
+        let vDirection = this.container.getAttribute('data-diretction');
 
         // POSIZIONO "NEL MEZZO" gli item non attivi
         for (const el of itemNotActiveArray) {
@@ -61,48 +68,51 @@ export class tGalleryItemClass {
         const swapItem = itemNotActiveArray.find((el) => {
             return el !== item;
         })
-        swapItem.classList.add('swap-item');
-
+        swapItem.classList.add('t-swap-item');
 
         // Posizione il precendete elemento attivo a dx o sx
         const activerItem = this.container.querySelector('.tGallery__item--active');
-        if (this.activeDirection == 'sx') {
+        if (this.activeHDirection == 'sx') {
             activerItem.style.order = 3;
-            swapItem.classList.add('swap-item--formLeft');
+            swapItem.classList.add('t-swap-item--formLeft');
         } else {
             activerItem.style.order = 1;
-            swapItem.classList.add('swap-item--formRight');
+            swapItem.classList.add('t-swap-item--formRight');
         }
 
         // Posiziono l'attuale elemento attivo
-
         if(itemPosX > this.center - 20) {
             currentInnerElement.classList.add('tg-form-right');
             currentInnerElement.classList.remove('tg-form-left')
-            this.activeDirection = 'sx'
+            this.activeHDirection = 'sx'
 
         } else {
             currentInnerElement.classList.add('tg-form-left');
             currentInnerElement.classList.remove('tg-form-right')
-            this.activeDirection = 'dx'
+            this.activeHDirection = 'dx'
         }
 
+        //CLONE
+        const swapClone = swapItem.cloneNode(true);
+        swapClone.classList.add('t-clone');
+        this.container.appendChild(swapClone);
+        (this.activeHDirection == 'sx') ? swapClone.classList.add('t-clone--sx') : swapClone.classList.add('t-clone--dx');
+        (vDirection == 'up') ? swapClone.classList.add('t-clone--down') : swapClone.classList.add('t-clone--up');
+
         // Setto l'altrnanza top/bottom
-        let direction = this.container.getAttribute('data-diretction');
-        (direction == 'up') ?  direction = 'down' :  direction = 'up';
+        (vDirection == 'up') ?  vDirection = 'down' :  vDirection = 'up';
+        this.container.setAttribute('data-diretction', vDirection )
 
         const scrollDestination = offset(this.container).top;
         bodyScrollTo(scrollDestination);
 
-        this.container.setAttribute('data-diretction', direction )
-
-        // REFRESH PREV ACTIVE ITEM STATUS
+        // RESET LAST ACTIVE ITEM
         activerItem.classList.remove('tGallery__item--active')
 
         // SET NEW ACTIVE ITEM
         item.classList.add('tGallery__item--active')
 
-        // REFRESH ITEM NEW NOT ACTIVE STATUS
+        // REFRESH ITEM NEW NOT ACTIVE WIDTH
         itemNotActive = this.container.querySelectorAll('.tGallery__item:not(.tGallery__item--active)');
         itemNotActiveArray = Array.from(itemNotActive);
 
@@ -112,6 +122,7 @@ export class tGalleryItemClass {
             innerEl.style.width = `${width/2}px`;
         }
 
+        // ACTIVE ITEM WIDTH
         currentInnerElement.style.width = `${width}px`;
     }
 }
