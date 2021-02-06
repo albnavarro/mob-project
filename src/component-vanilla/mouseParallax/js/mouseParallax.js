802dfbde424632931f7c82b0d91540bc1093013e
albertonav <alberto.navaro@gmail.com>
2021-02-06 21:26:10 +0100

    [ MOUSE PARALLAX ] new component

---
+++ src/component-vanilla/mouseParallax/js/mouseParallax.js
@@ -0,0 +1,43 @@
+import {eventManager} from "../../../js/base/eventManager.js";
+import {mouseParallaxItemClass} from "./mouseParallaxitem.js";
+
+class mouseParallaxClass {
+    constructor() {
+        this.mouseItem = document.querySelectorAll("*[data-conponent='m-comp--mouseParallax']");
+        this.instances = [];
+    }
+
+    init() {
+        eventManager.push('load', this.inzializeData.bind(this));
+    }
+
+    inzializeData() {
+        const itemArray = Array.from(this.mouseItem);
+        const dataArray = itemArray.map(item => {
+            return this.getItemData(item);
+        })
+
+        for (const item of dataArray) {
+            const mouseParallaxItem = new mouseParallaxItemClass(item);
+            this.instances.push(mouseParallaxItem);
+            mouseParallaxItem.init();
+        }
+    }
+
+    refresh() {
+        for (const item of this.instances) {
+            item.refresh();
+        }
+    }
+
+    getItemData(item) {
+        const data = {};
+        data.item = item
+        data.container = item.closest('.mouseParallax__container');
+        data.centerToViewoport = item.hasAttribute('data-centerToViewoport');
+        data.range = item.getAttribute('data-range') || 20
+        return data;
+    }
+}
+
+export const mouseParallax = new mouseParallaxClass()
---
+++ src/component-vanilla/mouseParallax/js/mouseParallaxitem.js
@@ -0,0 +1,66 @@
+import {mouseManager} from "../../../js/base/mouseManager.js";
+import {eventManager} from "../../../js/base/eventManager.js";
+import { outerHeight, outerWidth, offset } from "../../../js/utility/vanillaFunction.js";
+
+export class mouseParallaxItemClass {
+    constructor(data) {
+        this.item = data.item
+        this.centerToViewoport = data.centerToViewoport
+        this.range = data.range
+        this.container = data.container
+        this.height = 0
+        this.width = 0
+        this.offSetTop = 0
+        this.offSetLeft = 0
+    }
+
+    init() {
+        this.getDimension()
+        mouseManager.push('mousemove', () => this.onMove())
+        eventManager.push('resize', () => this.getDimension())
+        mouseManager.push('scroll', () => this.onMove())
+
+
+    }
+
+    getDimension() {
+        this.height = outerHeight(this.item)
+        this.width = outerWidth(this.item)
+        this.offSetTop = offset(this.container).top
+        this.offSetLeft = offset(this.container).left
+    }
+
+    onMove() {
+        let vw = 0
+        let vh = 0
+
+        if(this.centerToViewoport) {
+            vw = eventManager.windowsWidth()
+            vh = eventManager.windowsHeight()
+        } else {
+            vw = this.width
+            vh = this.height
+        }
+
+        let x = mouseManager.clientX()
+        let y = 0
+        ;(!this.centerToViewoport) ? y = mouseManager.pageY() : y = mouseManager.clientY()
+
+
+        let ax = 0
+        let ay = 0
+        if(this.centerToViewoport) {
+            ax = ( x - (vw / 2)) / this.range;
+            ay = ( y - (vh / 2)) / this.range;
+        } else {
+            ax = ((x - this.offSetLeft) - (vw / 2)) / this.range;
+            ay = ((y - this.offSetTop) - (vh / 2)) / this.range;
+        }
+
+        const style = {
+            'transform': `translate3D(${ax}px, ${ay}px, 0)`
+        }
+        Object.assign(this.item.style, style)
+
+    }
+}
