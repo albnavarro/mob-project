a0e1a736d8cb339dfa3962c08975210bd4486fc6
albertonav <alberto.navaro@gmail.com>
2021-03-01 20:13:13 +0100

    remove test gsap

--- src/js/test/gsapTest.js
+++ src/js/test/gsapTest.js
@@ -1,4 +1,4 @@
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 class gsapTestClass {
@@ -11,12 +11,13 @@ class gsapTestClass {
         gsap.registerPlugin(ScrollTrigger)
         this.triggerAnimation()
         this.triggerScroll()
+        this.parallax()
     }
 
     triggerAnimation() {
         const stagger = gsap.utils.toArray('.gaspT__inner--1 .gaspT__pin')
 
-        const tl = gsap.timeline({ paused: true }).to(stagger, .5,  {x: 100});
+        const tl = gsap.timeline({}).to(stagger, .5, {x: 100, stagger: .1});
 
         const trigger = ScrollTrigger.create({
             trigger: '.gaspT__inner--1',
@@ -35,18 +36,39 @@ class gsapTestClass {
         const stagger = gsap.utils.toArray('.gaspT__inner--2 .gaspT__pin')
 
         const pin = gsap.timeline({
-          scrollTrigger: {
-            trigger: '.gaspT__inner--2',
-            pin: true,
-            pinSpacing: true,
-            markers: true,
-            start: '-=100',
-            end: 'bottom top',
-            scrub: 0,
-          }
+            scrollTrigger: {
+                trigger: '.gaspT__inner--2',
+                pin: true,
+                pinSpacing: true,
+                markers: true,
+                start: '-=100',
+                end: 'bottom top',
+                scrub: 1, // ease velocity
+            }
         });
 
-        pin.to(stagger, { scale: .5, stagger: .5});
+        pin.to(stagger, {
+            ease: "slow(0.5, 0.8)",
+            scale: .5,
+            stagger: .5
+        });
+    }
+
+    parallax() {
+        gsap.utils.toArray(".gaspP").forEach((section, index) => {
+            gsap.to(this, {
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 100%",
+                    end: "bottom",
+                    scrub: 0,
+                    onUpdate: (self) => {
+                        section.style.setProperty("--progress", self.progress);
+                    }
+                }
+            });
+        });
+
     }
 
 }
