acb6cbbfb1f653fa03135a8ce0b439109f9ca381
albertonav <alberto.navaro@gmail.com>
2021-09-25 18:44:49 +0200

    [ move3D ] refine

--- src/component/move3D/js/move3Ditem.js
+++ src/component/move3D/js/move3Ditem.js
@@ -1,102 +1,106 @@
 export class move3DitemClass {
-    constructor(data) {
-        this.item = data.item;
-        this.container = data.container;
-        this.depth = data.depth;
-        // additional rotate
-        this.rotate = data.rotate;
-        this.direction = data.direction;
-        this.range = data.range;
-        this.initialRotate = data.initialRotate;
-        //
-        this.animate = data.animate;
-        this.width = 0;
-    }
+  constructor(data) {
+    this.item = data.item;
+    this.container = data.container;
+    this.depth = data.depth;
+    // additional rotate
+    this.rotate = data.rotate;
+    this.direction = data.direction;
+    this.range = data.range;
+    this.initialRotate = data.initialRotate;
+    //
+    this.animate = data.animate;
+    this.width = 0;
+  }
 
-    init() {
-        if (!this.animate) {
-            const style = {
-                transform: `translateZ(${this.depth}px)`,
-            };
-            Object.assign(this.item.style, style);
+  init() {
+    if (!this.animate) {
+      const style = {
+        transform: `translateZ(${this.depth}px)`,
+      };
+      Object.assign(this.item.style, style);
+    }
+  }
+
+  getRotate(range, delta, limit) {
+    return ((range * delta) / limit - this.initialRotate).toFixed(2);
+  }
+
+  move(delta, limit) {
+    let depth = this.depth;
+    depth = parseInt((this.depth * delta) / limit);
+
+    let rotateX = 0;
+    let rotateY = 0;
+
+    // X rotate
+    switch (this.rotate) {
+      case "x":
+        rotateX = this.getRotate(this.range, delta, limit);
+
+        switch (this.direction) {
+          case "bottom":
+            Object.assign(this.item.style, { "transform-origin": "top" });
+            rotateX = -rotateX;
+            break;
+
+          case "top":
+            Object.assign(this.item.style, { "transform-origin": "bottom" });
+            break;
         }
-    }
+        break;
 
-    getRotate(range, delta, limit) {
-        return ((range * delta) / limit - this.initialRotate).toFixed(2);
-    }
+      case "y":
+        rotateY = this.getRotate(this.range, delta, limit);
 
-    move(delta, limit) {
-        let depth = this.depth;
-        depth = parseInt((this.depth * delta) / limit);
+        switch (this.direction) {
+          case "left":
+            Object.assign(this.item.style, { "transform-origin": "right" });
+            rotateY = -rotateY;
+            break;
 
-        let rotateX = 0;
-        let rotateY = 0;
-
-        // X rotate
-        switch (this.rotate) {
-            case 'x':
-                rotateX = this.getRotate(this.range, delta, limit);
-
-                switch (this.direction) {
-                    case 'bottom':
-                        this.item.classList.add('origin-top');
-                        rotateX = -rotateX;
-                        break;
-
-                    case 'top':
-                        this.item.classList.add('origin-bottom');
-                        break;
-                }
-                break;
-
-            case 'y':
-                rotateY = this.getRotate(this.range, delta, limit);
-
-                switch (this.direction) {
-                    case 'left':
-                        this.item.classList.add('origin-right');
-                        rotateY = -rotateY;
-                        break;
-
-                    case 'right':
-                        this.item.classList.add('origin-left');
-                        break;
-                }
-                break;
-
-            case 'xy':
-                rotateY = this.getRotate(this.range, delta, limit);
-                rotateX = this.getRotate(this.range, delta, limit);
-
-                switch (this.direction) {
-                    case 'top-left':
-                        this.item.classList.add('origin-top-left');
-                        rotateY = -rotateY;
-                        break;
-
-                    case 'top-right':
-                        this.item.classList.add('origin-top-right');
-                        break;
-
-                    case 'bottom-left':
-                        this.item.classList.add('origin-bottom-left');
-                        rotateY = -rotateY;
-                        rotateX = -rotateX;
-                        break;
-
-                    case 'bottom-right':
-                        this.item.classList.add('origin-bottom-right');
-                        rotateX = -rotateX;
-                        break;
-                }
-                break;
+          case "right":
+            Object.assign(this.item.style, { "transform-origin": "left" });
+            break;
         }
+        break;
 
-        const style = {
-            transform: `translate3D(0,0,${depth}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
-        };
+      case "xy":
+        rotateY = this.getRotate(this.range, delta, limit);
+        rotateX = this.getRotate(this.range, delta, limit);
 
-        Object.assign(this.item.style, style);
+        switch (this.direction) {
+          case "top-left":
+            Object.assign(this.item.style, {
+              "transform-origin": "bottom right",
+            });
+            rotateY = -rotateY;
+            break;
+
+          case "top-right":
+            Object.assign(this.item.style, {
+              "transform-origin": "bottom left",
+            });
+            break;
+
+          case "bottom-left":
+            Object.assign(this.item.style, { "transform-origin": "top right" });
+            rotateY = -rotateY;
+            rotateX = -rotateX;
+            break;
+
+          case "bottom-right":
+            Object.assign(this.item.style, { "transform-origin": "top left" });
+            rotateX = -rotateX;
+            break;
+        }
+        break;
     }
+
+    const style = {
+      transform: `translate3D(0,0,${depth}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+    };
+
+    Object.assign(this.item.style, style);
+  }
 }
