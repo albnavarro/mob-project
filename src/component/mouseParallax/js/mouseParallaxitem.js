c44ae12ba8f9a09cd699aed505b68ad27d9ef693
albertonav <alberto.navaro@gmail.com>
2021-10-24 12:28:50 +0200

    mouseParallax add easing

--- src/component/mouseParallax/js/mouseParallaxitem.js
+++ src/component/mouseParallax/js/mouseParallaxitem.js
@@ -16,6 +16,11 @@ export class MouseParallaxItemClass {
         this.width = 0;
         this.offSetTop = 0;
         this.offSetLeft = 0;
+        this.endValue = { ax: 0, ay: 0 };
+        this.startValue = { ax: 0, ay: 0 };
+        this.smooth = 10;
+        this.prevValue = 0;
+        this.rafOnScroll = null;
     }
 
     init() {
@@ -36,40 +41,77 @@ export class MouseParallaxItemClass {
     }
 
     onMove() {
-        const {vw, vh} = (() => {
+        const { vw, vh } = (() => {
             if (this.centerToViewoport) {
                 return {
                     vw: eventManager.windowsWidth(),
-                    vh: eventManager.windowsHeight()
-                }
+                    vh: eventManager.windowsHeight(),
+                };
             } else {
                 return {
                     vw: this.width,
-                    vh: this.height
-                }
+                    vh: this.height,
+                };
             }
-        })()
+        })();
 
         const x = mouseManager.clientX();
-        const y = !this.centerToViewoport ? mouseManager.pageY() : mouseManager.clientY();
+        const y = !this.centerToViewoport
+            ? mouseManager.pageY()
+            : mouseManager.clientY();
 
-        const {ax, ay} = (() => {
+        const { ax, ay } = (() => {
             if (this.centerToViewoport) {
                 return {
                     ax: (x - vw / 2) / this.range,
-                    ay:(y - vh / 2) / this.range
-                }
+                    ay: (y - vh / 2) / this.range,
+                };
             } else {
                 return {
                     ax: (x - this.offSetLeft - vw / 2) / this.range,
-                    ay: (y - this.offSetTop - vh / 2) / this.range
-                }
+                    ay: (y - this.offSetTop - vh / 2) / this.range,
+                };
             }
-        })()
+        })();
 
-        const style = {
-            transform: `translate3D(${ax}px, ${ay}px, 0)`,
+        this.endValue = { ax, ay };
+
+        if (!this.rafOnScroll)
+            this.rafOnScroll = requestAnimationFrame(
+                this.onReuqestAnimScroll.bind(this)
+            );
+    }
+
+    onReuqestAnimScroll(timeStamp) {
+        const draw = (timeStamp) => {
+            console.log('tick');
+
+            this.prevValue = { ...this.startValue };
+
+            const { ax, ay } = (() => {
+                const v = this.smooth;
+                const { ax: axStart, ay: ayStart } = this.startValue;
+                const { ax: axEnd, ay: ayEnd } = this.endValue;
+
+                const ax = (axEnd - axStart) / v + axStart * 1;
+                const ay = (ayEnd - ayStart) / v + ayStart * 1;
+
+                return { ax: ax.toFixed(1), ay: ay.toFixed(1) };
+            })();
+
+            this.startValue = { ax, ay };
+            const { ax: axPrev, ay: ayPrev } = this.prevValue;
+            this.item.style.transform = `translate3D(${ax}px, ${ay}px, 0)`;
+
+            if (ax == axPrev && ay == ayPrev) {
+                cancelAnimationFrame(this.rafOnScroll);
+                this.rafOnScroll = null;
+                return;
+            }
+
+            this.rafOnScroll = requestAnimationFrame(draw);
         };
-        Object.assign(this.item.style, style);
+
+        draw(timeStamp);
     }
 }
