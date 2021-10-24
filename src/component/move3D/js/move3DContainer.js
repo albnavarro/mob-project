a9615e2e1411d181628376ba68268fdbff8027c0
albertonav <alberto.navaro@gmail.com>
2021-10-24 13:49:55 +0200

    move3D add ease

--- src/component/move3D/js/move3DContainer.js
+++ src/component/move3D/js/move3DContainer.js
@@ -1,255 +1,305 @@
-import { mouseManager } from "../../../js/base/mouseManager.js";
-import { eventManager } from "../../../js/base/eventManager.js";
-import { move3DitemClass } from "./move3Ditem.js";
+import { mouseManager } from '../../../js/base/mouseManager.js';
+import { eventManager } from '../../../js/base/eventManager.js';
+import { move3DitemClass } from './move3Ditem.js';
 import {
-  outerHeight,
-  outerWidth,
-  offset,
-} from "../../../js/utility/vanillaFunction.js";
+    outerHeight,
+    outerWidth,
+    offset,
+} from '../../../js/utility/vanillaFunction.js';
 
 export class move3DContainerClass {
-  constructor(data) {
-    this.item = data.item;
-    this.scene = this.item.querySelector(".move3D__scene");
-    this.container = this.item.querySelector(".move3D__container");
-    this.children = this.item.querySelectorAll(".move3D__item");
-    this.centerToViewoport = data.centerToViewoport;
-    this.perspective = data.perspective;
-    this.xDepth = data.xDepth;
-    this.yDepth = data.yDepth;
-    this.xLimit = data.xLimit;
-    this.yLimit = data.yLimit;
-    this.drag = data.drag;
-    this.height = 0;
-    this.width = 0;
-    this.offSetLeft = 0;
-    this.offSetTop = 0;
-    this.delta = 0;
-    this.limit = 0;
-    this.lastX = 0;
-    this.dragX = 0;
-    this.lastY = 0;
-    this.dragY = 0;
-    this.onDrag = false;
-    this.firstDrag = false;
-    this.pageY = false;
-    this.childrenInstances = [];
-  }
-
-  init() {
-    if (Modernizr.touchevents && !this.drag) return;
-
-    if (!this.centerToViewoport && !this.drag) this.pageY = true;
-
-    const itemArray = Array.from(this.children);
-    const dataArray = itemArray.map((item) => {
-      return this.getItemData(this.item, item);
-    });
-
-    for (const item of dataArray) {
-      const move3Ditem = new move3DitemClass(item);
-      this.childrenInstances.push(move3Ditem);
-      move3Ditem.init();
-    }
-
-    this.setDepth();
-    this.getDimension();
-
-    mouseManager.push("mousemove", () => this.onMove());
-    eventManager.push("resize", () => this.getDimension());
-
-    if (this.pageY) {
-      mouseManager.push("scroll", () => this.onMove());
-    }
-
-    if (this.drag) {
-      this.dragX = eventManager.windowsWidth() / 2;
-      this.dragY = eventManager.windowsHeight() / 2;
-      this.item.classList.add("move3D--drag");
-
-      if (Modernizr.touchevents) {
-        mouseManager.push("touchstart", () => this.onMouseDown());
-        mouseManager.push("touchend", () => this.onMouseUp());
-      } else {
-        mouseManager.push("mousedown", () => this.onMouseDown());
-        mouseManager.push("mouseup", () => this.onMouseUp());
-      }
-
-      mouseManager.push("touchmove", () => this.onMove());
-    }
-  }
-
-  getDimension() {
-    this.height = outerHeight(this.item);
-    this.width = outerWidth(this.item);
-    this.offSetLeft = offset(this.item).left;
-    this.offSetTop = offset(this.item).top;
-  }
-
-  getItemData(container, item) {
-    const data = {};
-    data.item = item;
-    data.container = container;
-    data.depth = item.getAttribute("data-depth") || 0;
-    // additional rotate
-    data.rotate = item.getAttribute("data-rotate") || null;
-    data.anchorPoint = item.getAttribute("data-anchorPoint") || null;
-    data.range = item.getAttribute("data-range") || 0;
-    data.initialRotate = item.getAttribute("data-initialRotate") || 0;
-    //
-    data.animate = item.getAttribute("data-animate") || 0;
-    return data;
-  }
-
-  setDepth() {
-    const style = {
-      perspective: `${this.perspective}px`,
-    };
-    Object.assign(this.scene.style, style);
-  }
-
-  onMove() {
-    const { vw, vh } = (() => {
-      if (this.centerToViewoport || this.drag) {
-        return {
-          vw: eventManager.windowsWidth(),
-          vh: eventManager.windowsHeight(),
-        };
-      } else {
-        return {
-          vw: this.width,
-          vh: this.height,
-        };
-      }
-    })();
-
-    const x = Modernizr.touchevents
-      ? mouseManager.pageX()
-      : mouseManager.clientX();
-
-    const y =
-      Modernizr.touchevents || this.pageY
-        ? mouseManager.pageY()
-        : mouseManager.clientY();
-
-    const { xgap, ygap } = (() => {
-      if (!this.onDrag) return { xgap: 0, ygap: 0 };
-
-      if (this.firstDrag) {
+    constructor(data) {
+        this.item = data.item;
+        this.scene = this.item.querySelector('.move3D__scene');
+        this.container = this.item.querySelector('.move3D__container');
+        this.children = this.item.querySelectorAll('.move3D__item');
+        this.centerToViewoport = data.centerToViewoport;
+        this.perspective = data.perspective;
+        this.xDepth = data.xDepth;
+        this.yDepth = data.yDepth;
+        this.xLimit = data.xLimit;
+        this.yLimit = data.yLimit;
+        this.drag = data.drag;
+        this.height = 0;
+        this.width = 0;
+        this.offSetLeft = 0;
+        this.offSetTop = 0;
+        this.delta = 0;
+        this.limit = 0;
+        this.lastX = 0;
+        this.dragX = 0;
+        this.lastY = 0;
+        this.dragY = 0;
+        this.onDrag = false;
         this.firstDrag = false;
-        return {
-          xgap: 0,
-          ygap: 0,
-        };
-      } else {
-        return {
-          xgap: x - this.lastX,
-          ygap: y - this.lastY,
-        };
-      }
-    })();
+        this.pageY = false;
+        this.childrenInstances = [];
 
-    if (this.onDrag) {
-      this.dragX += xgap;
-      this.dragY += ygap;
+        this.endValue = { axLimited: 0, ayLimited: 0 };
+        this.startValue = { axLimited: 0, ayLimited: 0 };
+        this.smooth = 10;
+        this.prevValue = 0;
+        this.rafOnScroll = null;
     }
 
-    const { xInMotion, yInMotion } = (() => {
-      if (this.onDrag) {
-        return {
-          xInMotion: this.dragX,
-          yInMotion: this.dragY,
-        };
-      } else {
-        return {
-          xInMotion: x,
-          yInMotion: y,
-        };
-      }
-    })();
+    init() {
+        if (Modernizr.touchevents && !this.drag) return;
 
-    /*
+        if (!this.centerToViewoport && !this.drag) this.pageY = true;
+
+        const itemArray = Array.from(this.children);
+        const dataArray = itemArray.map((item) => {
+            return this.getItemData(this.item, item);
+        });
+
+        for (const item of dataArray) {
+            const move3Ditem = new move3DitemClass(item);
+            this.childrenInstances.push(move3Ditem);
+            move3Ditem.init();
+        }
+
+        this.setDepth();
+        this.getDimension();
+
+        mouseManager.push('mousemove', () => this.onMove());
+        eventManager.push('resize', () => this.getDimension());
+
+        if (this.pageY) {
+            mouseManager.push('scroll', () => this.onMove());
+        }
+
+        if (this.drag) {
+            this.dragX = eventManager.windowsWidth() / 2;
+            this.dragY = eventManager.windowsHeight() / 2;
+            this.item.classList.add('move3D--drag');
+
+            if (Modernizr.touchevents) {
+                mouseManager.push('touchstart', () => this.onMouseDown());
+                mouseManager.push('touchend', () => this.onMouseUp());
+            } else {
+                mouseManager.push('mousedown', () => this.onMouseDown());
+                mouseManager.push('mouseup', () => this.onMouseUp());
+            }
+
+            mouseManager.push('touchmove', () => this.onMove());
+        }
+    }
+
+    getDimension() {
+        this.height = outerHeight(this.item);
+        this.width = outerWidth(this.item);
+        this.offSetLeft = offset(this.item).left;
+        this.offSetTop = offset(this.item).top;
+    }
+
+    getItemData(container, item) {
+        const data = {};
+        data.item = item;
+        data.container = container;
+        data.depth = item.getAttribute('data-depth') || 0;
+        // additional rotate
+        data.rotate = item.getAttribute('data-rotate') || null;
+        data.anchorPoint = item.getAttribute('data-anchorPoint') || null;
+        data.range = item.getAttribute('data-range') || 0;
+        data.initialRotate = item.getAttribute('data-initialRotate') || 0;
+        //
+        data.animate = item.getAttribute('data-animate') || 0;
+        return data;
+    }
+
+    setDepth() {
+        const style = {
+            perspective: `${this.perspective}px`,
+        };
+        Object.assign(this.scene.style, style);
+    }
+
+    onMove() {
+        const { vw, vh } = (() => {
+            if (this.centerToViewoport || this.drag) {
+                return {
+                    vw: eventManager.windowsWidth(),
+                    vh: eventManager.windowsHeight(),
+                };
+            } else {
+                return {
+                    vw: this.width,
+                    vh: this.height,
+                };
+            }
+        })();
+
+        const x = Modernizr.touchevents
+            ? mouseManager.pageX()
+            : mouseManager.clientX();
+
+        const y =
+            Modernizr.touchevents || this.pageY
+                ? mouseManager.pageY()
+                : mouseManager.clientY();
+
+        const { xgap, ygap } = (() => {
+            if (!this.onDrag) return { xgap: 0, ygap: 0 };
+
+            if (this.firstDrag) {
+                this.firstDrag = false;
+                return {
+                    xgap: 0,
+                    ygap: 0,
+                };
+            } else {
+                return {
+                    xgap: x - this.lastX,
+                    ygap: y - this.lastY,
+                };
+            }
+        })();
+
+        if (this.onDrag) {
+            this.dragX += xgap;
+            this.dragY += ygap;
+        }
+
+        const { xInMotion, yInMotion } = (() => {
+            if (this.onDrag) {
+                return {
+                    xInMotion: this.dragX,
+                    yInMotion: this.dragY,
+                };
+            } else {
+                return {
+                    xInMotion: x,
+                    yInMotion: y,
+                };
+            }
+        })();
+
+        /*
         ax = grado di rotazione sull'asse X
         ay = grado di rotazione sull'asse Y
         */
-    const { ax, ay } = (() => {
-      if (this.centerToViewoport || this.drag) {
-        return {
-          ax: -(vw / 2 - xInMotion) / this.xDepth,
-          ay: (vh / 2 - yInMotion) / this.yDepth,
-        };
-      } else {
-        return {
-          ax: -(vw / 2 - (xInMotion - this.offSetLeft)) / this.xDepth,
-          ay: (vh / 2 - (yInMotion - this.offSetTop)) / this.yDepth,
-        };
-      }
-    })();
+        const { ax, ay } = (() => {
+            if (this.centerToViewoport || this.drag) {
+                return {
+                    ax: -(vw / 2 - xInMotion) / this.xDepth,
+                    ay: (vh / 2 - yInMotion) / this.yDepth,
+                };
+            } else {
+                return {
+                    ax: -(vw / 2 - (xInMotion - this.offSetLeft)) / this.xDepth,
+                    ay: (vh / 2 - (yInMotion - this.offSetTop)) / this.yDepth,
+                };
+            }
+        })();
 
-    const xlimitReached = Math.abs(ax) > this.xLimit;
-    const ylimitReached = Math.abs(ay) > this.yLimit;
+        const xlimitReached = Math.abs(ax) > this.xLimit;
+        const ylimitReached = Math.abs(ay) > this.yLimit;
 
-    const axLimited = (() => {
-      if (!xlimitReached) return ax;
-      return ax > 0 ? this.xLimit : -this.xLimit;
-    })();
+        const axLimited = (() => {
+            if (!xlimitReached) return ax;
+            return ax > 0 ? this.xLimit : -this.xLimit;
+        })();
 
-    const ayLimited = (() => {
-      if (!ylimitReached) return ay;
-      return ay > 0 ? this.yLimit : -this.yLimit;
-    })();
+        const ayLimited = (() => {
+            if (!ylimitReached) return ay;
+            return ay > 0 ? this.yLimit : -this.yLimit;
+        })();
 
-    // TODO: calcolare il valore x y corrspondente all 'angolo limite e assegnarlo
-    if (xlimitReached) this.dragX -= xgap;
-    if (ylimitReached) this.dragY -= ygap;
+        // TODO: calcolare il valore x y corrspondente all 'angolo limite e assegnarlo
+        if (xlimitReached) this.dragX -= xgap;
+        if (ylimitReached) this.dragY -= ygap;
 
-    this.lastX = x;
-    this.lastY = y;
+        this.lastX = x;
+        this.lastY = y;
 
-    /*
+        /*
         Calcolo il valore da passare ai componenti figli per animarre l'asse Z.
         Il delta sarà l'ipotenusa del triangolo formato dai volri ax e ay
         */
-    this.delta = Math.sqrt(
-      Math.pow(Math.abs(ayLimited), 2) + Math.pow(Math.abs(axLimited), 2)
-    );
+        this.delta = Math.sqrt(
+            Math.pow(Math.abs(ayLimited), 2) + Math.pow(Math.abs(axLimited), 2)
+        );
 
-    this.limit = Math.sqrt(
-      Math.pow(Math.abs(this.xLimit), 2) + Math.pow(Math.abs(this.yLimit), 2)
-    );
+        this.limit = Math.sqrt(
+            Math.pow(Math.abs(this.xLimit), 2) +
+                Math.pow(Math.abs(this.yLimit), 2)
+        );
 
-    const apply = (this.drag && this.onDrag) || !this.drag ? true : false;
+        const apply = (this.drag && this.onDrag) || !this.drag ? true : false;
 
-    if (apply) {
-      const style = {
-        transform: `rotateY(${axLimited}deg) rotateX(${ayLimited}deg) translateZ(0)`,
-      };
-      Object.assign(this.container.style, style);
+        if (apply) {
+            this.endValue = { axLimited, ayLimited };
 
-      // Children
-      for (const item of this.childrenInstances) {
-        if (item.animate) item.move(this.delta, this.limit);
-      }
+            if (!this.rafOnScroll)
+                this.rafOnScroll = requestAnimationFrame(
+                    this.onReuqestAnimScroll.bind(this)
+                );
+
+            // Children
+            for (const item of this.childrenInstances) {
+                if (item.animate) item.move(this.delta, this.limit);
+            }
+        }
     }
-  }
 
-  draggable() {
-    return (
-      mouseManager.pageY() > this.offSetTop &&
-      mouseManager.pageY() < this.offSetTop + this.height &&
-      mouseManager.pageX() > this.offSetLeft &&
-      mouseManager.pageX() < this.offSetLeft + this.width
-    );
-  }
+    onReuqestAnimScroll(timeStamp) {
+        const draw = (timeStamp) => {
+            // console.log('tick');
 
-  onMouseDown() {
-    if (this.draggable()) {
-      this.onDrag = true;
-      this.firstDrag = true;
+            this.prevValue = { ...this.startValue };
+
+            const { axLimited, ayLimited } = (() => {
+                const v = this.smooth;
+                const { axLimited: axLimitedStart, ayLimited: ayLimitedStart } =
+                    this.startValue;
+                const { axLimited: axLimitedEnd, ayLimited: ayLimitedEnd } =
+                    this.endValue;
+
+                const axLimited =
+                    (axLimitedEnd - axLimitedStart) / v + axLimitedStart * 1;
+                const ayLimited =
+                    (ayLimitedEnd - ayLimitedStart) / v + ayLimitedStart * 1;
+
+                return {
+                    axLimited: axLimited.toFixed(1),
+                    ayLimited: ayLimited.toFixed(1),
+                };
+            })();
+
+            this.startValue = { axLimited, ayLimited };
+            const { axLimited: axPrev, ayLimited: ayPrev } = this.prevValue;
+
+            this.container.style.transform = `rotateY(${axLimited}deg) rotateX(${ayLimited}deg) translateZ(0)`;
+
+            if (axLimited == axPrev && ayLimited == ayPrev) {
+                cancelAnimationFrame(this.rafOnScroll);
+                this.rafOnScroll = null;
+                return;
+            }
+
+            this.rafOnScroll = requestAnimationFrame(draw);
+        };
+
+        draw(timeStamp);
     }
-  }
 
-  onMouseUp() {
-    this.onDrag = false;
-  }
+    draggable() {
+        return (
+            mouseManager.pageY() > this.offSetTop &&
+            mouseManager.pageY() < this.offSetTop + this.height &&
+            mouseManager.pageX() > this.offSetLeft &&
+            mouseManager.pageX() < this.offSetLeft + this.width
+        );
+    }
+
+    onMouseDown() {
+        if (this.draggable()) {
+            this.onDrag = true;
+            this.firstDrag = true;
+        }
+    }
+
+    onMouseUp() {
+        this.onDrag = false;
+    }
 }
--- src/component/move3D/js/move3Ditem.js
+++ src/component/move3D/js/move3Ditem.js
@@ -1,51 +1,111 @@
-import { move3DUtils } from "./move3Dutils.js";
+import { move3DUtils } from './move3Dutils.js';
 
 export class move3DitemClass {
-  constructor(data) {
-    this.item = data.item;
-    this.depth = data.depth;
-    this.rotate = data.rotate;
-    this.anchorPoint = data.anchorPoint;
-    this.range = data.range;
-    this.initialRotate = data.initialRotate;
-    this.animate = data.animate;
-  }
+    constructor(data) {
+        this.item = data.item;
+        this.depth = data.depth;
+        this.rotate = data.rotate;
+        this.anchorPoint = data.anchorPoint;
+        this.range = data.range;
+        this.initialRotate = data.initialRotate;
+        this.animate = data.animate;
 
-  init() {
-    if (!this.animate) {
-      const style = {
-        transform: `translateZ(${this.depth}px)`,
-      };
-      Object.assign(this.item.style, style);
+        this.endValue = { depth: 0, rotateX: 0, rotateY: 0 };
+        this.startValue = { depth: 0, rotateX: 0, rotateY: 0 };
+        this.smooth = 10;
+        this.prevValue = 0;
+        this.rafOnScroll = null;
     }
-  }
 
-  move(delta, limit) {
-    const depth = parseInt((this.depth * delta) / limit);
-    const getRotateData = {
-      startRotation: this.initialRotate,
-      range: this.range,
-      delta: delta,
-      limit: limit,
-    };
-    const baseRotateX = move3DUtils.getRotate(getRotateData);
-    const baseRotateY = move3DUtils.getRotate(getRotateData);
+    init() {
+        if (!this.animate) {
+            const style = {
+                transform: `translateZ(${this.depth}px)`,
+            };
+            Object.assign(this.item.style, style);
+        }
+    }
 
-    const getRotateFromPositionData = {
-      rotate: this.rotate,
-      anchorPoint: this.anchorPoint,
-      item: this.item,
-      baseRotateX,
-      baseRotateY,
-    };
-    const { rotateX, rotateY } = move3DUtils.getRotateFromPosition(
-      getRotateFromPositionData
-    );
+    move(delta, limit) {
+        const depth = parseInt((this.depth * delta) / limit);
+        const getRotateData = {
+            startRotation: this.initialRotate,
+            range: this.range,
+            delta: delta,
+            limit: limit,
+        };
+        const baseRotateX = move3DUtils.getRotate(getRotateData);
+        const baseRotateY = move3DUtils.getRotate(getRotateData);
 
-    const style = {
-      transform: `translate3D(0,0,${depth}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
-    };
+        const getRotateFromPositionData = {
+            rotate: this.rotate,
+            anchorPoint: this.anchorPoint,
+            item: this.item,
+            baseRotateX,
+            baseRotateY,
+        };
+        const { rotateX, rotateY } = move3DUtils.getRotateFromPosition(
+            getRotateFromPositionData
+        );
 
-    Object.assign(this.item.style, style);
-  }
+        this.endValue = { depth, rotateX, rotateY };
+
+        if (!this.rafOnScroll)
+            this.rafOnScroll = requestAnimationFrame(
+                this.onReuqestAnimScroll.bind(this)
+            );
+    }
+
+    onReuqestAnimScroll(timeStamp) {
+        const draw = (timeStamp) => {
+            // console.log('tick');
+
+            this.prevValue = { ...this.startValue };
+
+            const { depth, rotateX, rotateY } = (() => {
+                const v = this.smooth;
+                const {
+                    depth: depthStart,
+                    rotateX: rotateXStart,
+                    rotateY: rotateYStart,
+                } = this.startValue;
+                const {
+                    depth: depthEnd,
+                    rotateX: rotateXEnd,
+                    rotateY: rotateYEnd,
+                } = this.endValue;
+
+                const depth = (depthEnd - depthStart) / v + depthStart * 1;
+                const rotateX =
+                    (rotateXEnd - rotateXStart) / v + rotateXStart * 1;
+                const rotateY =
+                    (rotateYEnd - rotateYStart) / v + rotateYStart * 1;
+
+                return {
+                    depth: depth.toFixed(1),
+                    rotateX: rotateX.toFixed(1),
+                    rotateY: rotateY.toFixed(1),
+                };
+            })();
+
+            this.startValue = { depth, rotateX, rotateY };
+            const {
+                depth: depthPrev,
+                rotateX: axPrev,
+                rotateY: ayPrev,
+            } = this.prevValue;
+
+            this.item.style.transform = `translate3D(0,0,${depth}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+            if (depth == depthPrev && rotateX == axPrev && rotateY == ayPrev) {
+                cancelAnimationFrame(this.rafOnScroll);
+                this.rafOnScroll = null;
+                return;
+            }
+
+            this.rafOnScroll = requestAnimationFrame(draw);
+        };
+
+        draw(timeStamp);
+    }
 }
