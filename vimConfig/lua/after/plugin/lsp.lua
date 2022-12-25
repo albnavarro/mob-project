ad0a612592ec578a8b82354f206b71c401dde8f3
albertonav <alberto.navaro@gmail.com>
2022-12-25 23:49:56 +0100

    lua config back to formatter instad null-ls

--- vimConfig/lua/after/plugin/lsp.lua
+++ vimConfig/lua/after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 local lsp = require("lsp-zero")
-local null_ls = require('null-ls')
+-- local null_ls = require('null-ls')
 
 lsp.preset("recommended")
 
@@ -30,28 +30,28 @@ lsp.configure('stylelint_lsp', {
 
 lsp.setup()
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_opts = lsp.build_options('null-ls', {})
-
-null_ls.setup({
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylelint
-    }
-})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local null_opts = lsp.build_options('null-ls', {})
+--
+-- null_ls.setup({
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+--                     vim.lsp.buf.format({ bufnr = bufnr })
+--                 end,
+--             })
+--         end
+--     end,
+--     sources = {
+--         null_ls.builtins.formatting.prettier,
+--         null_ls.builtins.formatting.stylelint
+--     }
+-- })
 
 vim.diagnostic.config({
     virtual_text = true,
