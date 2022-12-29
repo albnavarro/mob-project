1c0bd4e924615ebc2a36bbddfa3e2796ca0191a4
albertonav <alberto.navaro@gmail.com>
2022-12-29 20:44:28 +0100

    nvim telescope keymap

--- vimConfig/lua/after/plugin/telescope.lua
+++ vimConfig/lua/after/plugin/telescope.lua
@@ -7,7 +7,7 @@ vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
-vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<C-h>', builtin.oldfiles, {})
 
 require('telescope').setup{
     defaults = {
