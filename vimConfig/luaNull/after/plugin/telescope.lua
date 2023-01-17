b28b1b800dbd7c9883c3701ac73189fc48d1bf08
albertonav <alberto.navaro@gmail.com>
2023-01-17 23:05:26 +0100

    Telescope map

--- vimConfig/luaNull/after/plugin/telescope.lua
+++ vimConfig/luaNull/after/plugin/telescope.lua
@@ -10,10 +10,11 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fj", builtin.jumplist, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fr", builtin.registers, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<C-h>", builtin.oldfiles, {})
-vim.keymap.set("n", "<C-m>", builtin.jumplist, {})
 
 telescope.setup({
 	defaults = {
