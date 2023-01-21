1ae634dc83690e236a24514090f637e22deda0b8
albertonav <alberto.navaro@gmail.com>
2023-01-21 17:15:49 +0100

    Add new telescope keymap

--- vimConfig/luaNull/after/plugin/telescope.lua
+++ vimConfig/luaNull/after/plugin/telescope.lua
@@ -14,6 +14,7 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<leader>fj", builtin.jumplist, {})
+vim.keymap.set("n", "<leader>fm", builtin.marks, {})
 vim.keymap.set("n", "<C-h>", builtin.oldfiles, {})
 
 telescope.setup({
