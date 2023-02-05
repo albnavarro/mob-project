924ce74cbb4f1c1400ab7e44cab008a490c5cd79
albertonav <alberto.navaro@gmail.com>
2023-02-05 14:25:09 +0100

    nvim-cmp add in source.buffer vsible buffer option

--- vimConfig/luaClean/after/plugin/lps/cmp.lua
+++ vimConfig/luaClean/after/plugin/lps/cmp.lua
@@ -37,7 +37,20 @@ local cmp_config = {
 	sources = {
 		{ name = "path" },
 		{ name = "nvim_lsp", keyword_length = 3 },
-		{ name = "buffer", keyword_length = 3 },
+		{
+			name = "buffer",
+			keyword_length = 3,
+			-- use on visible buffer
+			option = {
+				get_bufnrs = function()
+					local bufs = {}
+					for _, win in ipairs(vim.api.nvim_list_wins()) do
+						bufs[vim.api.nvim_win_get_buf(win)] = true
+					end
+					return vim.tbl_keys(bufs)
+				end,
+			},
+		},
 		{ name = "luasnip", keyword_length = 2 },
 	},
 	window = {
