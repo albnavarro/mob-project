a6a87931f307d70311bc8a198e000532417cc055
albertonav <alberto.navaro@gmail.com>
2023-02-20 21:11:44 +0100

    nvim - smartSearch fix naming

--- vimConfig/luaClean/lua/mobbu/smartSearch.lua
+++ vimConfig/luaClean/lua/mobbu/smartSearch.lua
@@ -1,81 +1,82 @@
-local v = vim
-
--- Search status
-local smart_search_is_active = false
-
--- Initial scrolloff value
-local scrolloffValue = v.api.nvim_get_option("scrolloff")
+local V = vim
+local A = V.api
+local M = {
+	concatenateRegex = [[.\{-}]],
+	active = false,
+	initialScrollOff = A.nvim_get_option("scrolloff"),
+}
 
 -- Termcode convert utils
 local function t(str)
-	return v.api.nvim_replace_termcodes(str, true, true, true)
+	return A.nvim_replace_termcodes(str, true, true, true)
 end
 
-function Setup_smart_search()
-	smart_search_is_active = true
-	v.cmd(':let @/ = ""')
-	v.cmd(":set hlsearch")
+-- setup smart_search settings
+local function initSettings()
+	M.active = true
+	V.cmd(':let @/ = ""')
+	V.cmd(":set hlsearch")
 end
 
-function Clear_smart_search()
-	if smart_search_is_active == true then
-		smart_search_is_active = false
-		v.cmd(":set nohlsearch")
+-- remove smart_search settings
+local function removeSettings()
+	if M.active == true then
+		M.active = false
+		V.cmd(":set nohlsearch")
 	end
 end
 
-function Command_space_pressed()
-	if smart_search_is_active == true then
-		return [[.\{-}]]
-	else
-		return t("<Space>")
-	end
+-- return concatenateRegex on space press or default space
+local function onSpacePressed()
+	return (M.active == true) and M.concatenateRegex or t("<Space>")
 end
 
-function Get_line_num()
-	return v.api.nvim_win_get_cursor(0)[1]
+-- get current line number
+local function getCurrentLineNumber()
+	return A.nvim_win_get_cursor(0)[1]
 end
 
-function Get_window_line(use_first_line)
-	if use_first_line == true then
-		return v.api.nvim_win_call(0, function()
-			return v.fn.line("w0") + scrolloffValue
+-- get top/bottom windows line
+local function getWindowLineNumber(useFirstLine)
+	if useFirstLine == true then
+		return A.nvim_win_call(0, function()
+			return V.fn.line("w0") + M.initialScrollOff
 		end)
 	else
-		return v.api.nvim_win_call(0, function()
-			return v.fn.line("w$") - scrolloffValue
+		return A.nvim_win_call(0, function()
+			return V.fn.line("w$") - M.initialScrollOff
 		end)
 	end
 end
 
-v.api.nvim_set_keymap("n", "<Leader>d", "", {
+A.nvim_set_keymap("n", "<Leader>d", "", {
 	noremap = true,
 	silent = false,
 	expr = true,
 	callback = function()
-		Setup_smart_search()
-		return [[/\c\%>]] .. Get_line_num() - 1 .. [[l\%<]] .. Get_window_line(false) .. [[l]] .. t("<C-b>")
+		initSettings()
+		return [[/\c\%>]] .. getCurrentLineNumber() - 1 .. [[l\%<]] .. getWindowLineNumber(false) .. [[l]] .. t("<C-b>")
 	end,
 })
 
-v.api.nvim_set_keymap("n", "<Leader>u", "", {
+A.nvim_set_keymap("n", "<Leader>u", "", {
 	noremap = true,
 	expr = true,
 	callback = function()
-		Setup_smart_search()
-		return [[?\c\%<]] .. Get_line_num() - 1 .. [[l\%>]] .. Get_window_line(true) .. [[l]] .. t("<C-b>")
+		initSettings()
+		return [[?\c\%<]] .. getCurrentLineNumber() + 1 .. [[l\%>]] .. getWindowLineNumber(true) .. [[l]] .. t("<C-b>")
 	end,
 })
 
-v.api.nvim_set_keymap("c", "<Space>", "", {
+A.nvim_set_keymap("c", "<Space>", "", {
 	noremap = true,
 	expr = true,
-	callback = Command_space_pressed,
+	callback = onSpacePressed,
 })
 
 -- clear setting on commandline leave
-local smartSearchGrp = v.api.nvim_create_augroup("SearchConcatenate", { clear = true })
-v.api.nvim_create_autocmd("CmdlineLeave", {
-	callback = Clear_smart_search,
+local smartSearchGrp = A.nvim_create_augroup("SearchConcatenate", { clear = true })
+A.nvim_create_autocmd("CmdlineLeave", {
+	callback = removeSettings,
 	group = smartSearchGrp,
 })
